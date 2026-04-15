344e9430e1dc07258b126c5c40efa1cffdd694d5
magdaaa <magdaaa@eruditor-group.com>
2026-04-15 16:11:01 +0300

    initial: expo 52 + netinfo, fingerprint.json generated

---
+++ index.js
@@ -0,0 +1,12 @@
+import { registerRootComponent } from 'expo';
+import { Text, View } from 'react-native';
+
+function App() {
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+}
+
+registerRootComponent(App);
